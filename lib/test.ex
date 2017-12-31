49308889fd9a1ef2143bad4ec98545364ce73b6b
Luiz Parreira <luizfilipester@gmail.com>
2017-12-31 12:36:06 -0200

    Improved test's signature

--- lib/test.ex
+++ lib/test.ex
@@ -1,13 +1,13 @@
 defmodule Test do
-  def test(n, {game, memory, policy}) do
+  def test(n, {game, policy, memory}) do
     case Game.final?(game) do
-      true -> {:ok, game}
+      true -> game
       false ->
         state = Game.state(game)
         action_values = Utils.build_action_values(game, memory, state)
         action = Policy.choose(policy, action_values)
         game = Game.act(game, action)
-        test_market(game, memory, policy)
+        test(n, {game, policy, memory})
     end
   end
 end
